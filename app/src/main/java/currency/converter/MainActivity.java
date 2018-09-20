3894b4d5148d3fe363be215c75ffa2cead1010cb
Annika Ronkainen <t5roan00@students.oamk.fi>
2018-09-20 13:46:19 +0300

    Base

--- app/src/main/java/currency/converter/MainActivity.java
+++ app/src/main/java/currency/converter/MainActivity.java
@@ -2,6 +2,7 @@ package currency.converter;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("TESTI", "Testilisäys");
     }
 }
