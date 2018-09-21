fdbff77554df56c5996ddcb3885e56b0c33e6b0b
Annika Ronkainen <t5roan00@students.oamk.fi>
2018-09-21 19:53:22 +0300

    Functionality for selected listview item. Maybe this could be made little bit prettier.

--- app/src/main/java/currency/converter/CalculatorActivity.java
+++ app/src/main/java/currency/converter/CalculatorActivity.java
@@ -3,6 +3,8 @@ package currency.converter;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class CalculatorActivity extends Activity {
@@ -10,6 +12,16 @@ public class CalculatorActivity extends Activity {
     // XML node keys
     static final String KEY_CURRENCY = "currency";
     static final String KEY_RATE = "rate";
+
+    private TextView resultEUR;
+    private TextView resultCUR;
+
+    private EditText currencyAmount;
+    private EditText eurAmount;
+
+    public String currency;
+    public String rate;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,14 +32,40 @@ public class CalculatorActivity extends Activity {
         Intent in = getIntent();
 
         // Get XML values from previous intent
-        String homeN = in.getStringExtra(KEY_CURRENCY);
-        String visitorN = in.getStringExtra(KEY_RATE);
+        currency = in.getStringExtra(KEY_CURRENCY);
+        rate = in.getStringExtra(KEY_RATE);
 
-        // Displaying all values on the screen
-        TextView hName = findViewById(R.id.home_name);
-        TextView vName = findViewById(R.id.visitor_name);
+        TextView cName1 = findViewById(R.id.currency_name1);
+        TextView cName2 = findViewById(R.id.currency_name2);
+        TextView rName = findViewById(R.id.rate_value);
+        TextView cName = findViewById(R.id.textViewCurrency);
+        resultEUR = findViewById(R.id.resultEUR);
+        resultCUR = findViewById(R.id.resultCUR);
+        currencyAmount = findViewById(R.id.valueInput1);
+        eurAmount = findViewById(R.id.valueInputEUR);
 
-        hName.setText(homeN);
-        vName.setText(visitorN);
+        cName.setText(currency);
+        cName1.setText(currency);
+        cName2.setText(currency);
+        rName.setText(rate);
+        resultCUR.setText(rate);
+        float calculation = 1 / Float.valueOf(String.valueOf(rate));
+        resultEUR.setText(Float.toString(calculation));
+    }
+
+    public void calculateCUR(View view) {
+        float value1 = Float.valueOf(rate);
+        float value2 = Float.valueOf(String.valueOf(eurAmount.getText()));
+        float calculation = value1 * value2;
+        String result = Float.toString(calculation);
+        resultCUR.setText(result);
+    }
+
+    public void calculateEUR(View view) {
+        float value1 = Float.valueOf(rate);
+        float value2 = Float.valueOf(String.valueOf(currencyAmount.getText()));
+        float calculation = value2 / value1;
+        String result = Float.toString(calculation);
+        resultEUR.setText(result);
     }
 }
